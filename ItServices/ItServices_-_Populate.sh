77847ea741402bf18938f584bfb56fe528cbea04
Luiz Sales <luiz@lsales.biz>
2015-08-11 09:43:38 -0700

    Inclusao de Menu, help, Case

--- ItServices/ItServices_-_Populate.sh
+++ ItServices/ItServices_-_Populate.sh
@@ -4,15 +4,18 @@
 
 
 #HOSTNAME=''
-API=' ' # http://localhost/api_jsonrpc.php
+#API=' ' # http://localhost/api_jsonrpc.php
+API='http://zbx.lsales.biz/api_jsonrpc.php'
+ZABBIX_USER='redhate'
+ZABBIX_PASS='r1ed98hat2e'
 
 # CONSTANT VARIABLES
-ZABBIX_USER=''
-ZABBIX_PASS=''
+#ZABBIX_USER=''
+#ZABBIX_PASS=''
 
-HOSTGROUP=$1
-HOSTN=$2
-NOMEITEM=$3
+HOSTGROUP=$2
+HOSTN=$3
+NOMEITEM=$4
 
 authenticate()
 {
@@ -45,7 +48,6 @@ get_hostgroups() {
 get_hostgroups_id() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"hostgroup.get\",
         \"params\": {
@@ -65,7 +67,6 @@ GRPID=$(get_hostgroups_id)
 get_hosts_hostgroups() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"host.get\",
         \"params\": {
@@ -81,7 +82,6 @@ get_hosts_hostgroups() {
 get_host_id() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"host.get\",
         \"params\": {
@@ -102,7 +102,6 @@ HOSTID=$(get_host_id);
 get_items_hosts() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"item.get\",
         \"params\": {
@@ -112,7 +111,7 @@ get_items_hosts() {
 	\"selectTriggers\" : \"extend\"
         },
         \"auth\": \"$AUTH_TOKEN\",
-        \"id\": 1}"  #| awk -v RS=',"' -F: '/^name/ {print $2}'
+        \"id\": 1}"  | awk -v RS=',"' -F: '/^name/ {print $2}'
 }
 #echo "Todos os Items com Trigger de $2"
 #get_items_hosts;
@@ -121,7 +120,6 @@ get_items_hosts() {
 get_item_triggerid() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"item.get\",
         \"params\": {
@@ -141,7 +139,6 @@ ITEM_TRIGID=$(get_item_triggerid)
 get_triggers_hosts() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"trigger.get\",
         \"params\": {
@@ -152,7 +149,7 @@ get_triggers_hosts() {
 	\"expandExpression\" : \"1\"
 	},
         \"auth\": \"$AUTH_TOKEN\",
-        \"id\": 1}"  #| awk -v RS=',"' -F: '/^name/ {print $2}'
+        \"id\": 1}"  | awk -v RS=',"' -F: '/^description/ {print $2}'
 }
 #echo "Todos as triggers do host $2"
 #get_triggers_hosts;
@@ -160,7 +157,6 @@ get_triggers_hosts() {
 mk_father_itservices() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"service.create\",
         \"params\": {
@@ -179,7 +175,6 @@ mk_father_itservices() {
 get_itservices() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
 	\"jsonrpc\": \"2.0\",
 	\"method\": \"service.get\",
 	\"params\": {
@@ -195,7 +190,6 @@ get_itservices() {
 get_its_pid() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"service.get\",
         \"params\": {
@@ -215,7 +209,6 @@ ITS_PID=$(get_its_pid | awk -v RS='{"' -F: '/^serviceid/ {print $2}' | awk -F\" 
 mk_child_itservices() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"service.create\",
         \"params\": {
@@ -235,7 +228,6 @@ mk_child_itservices() {
 get_its_pid_child() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"service.get\",
         \"params\": {
@@ -256,7 +248,6 @@ ITS_PID_CHILD=$(get_its_pid_child | awk -v RS='{"' -F: '/^serviceid/ {print $2}'
 mk_child_its_trigger() {
 
   wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
-
         \"jsonrpc\": \"2.0\",
         \"method\": \"service.create\",
         \"params\": {
@@ -273,21 +264,85 @@ mk_child_its_trigger() {
 }
 #mk_child_its_trigger;
 
+mk_populate() {
 # Inicio do Laço Populate
-for HOSTGROUP in `get_hostgroups;`
+for HOSTGROUP in `get_hostgroups`
 do
 	echo 1
 	mk_father_itservices;
-	for HOSTN in `get_hosts_hostgroups;`
+	#echo `get_hosts_hostgroups "$HOSTGROUP" `
+	echo 2
+	filho=$(get_hosts_hostgroups "$HOSTGROUP")
+	for HOSTN in `echo "$filho"`
 	do
-		echo 2
-		mk_child_itservices $HOSTGROUP $HOSTN	
-		for NOMEITEM in `get_items_hosts;`
-		do
-			echo 3
-			mk_child_its_trigger $HOSTGROUP $HOSTN $NOMEITEM
-		done
+		echo 3
+		cria_filho=$(mk_child_itservices "$HOSTGROUP" "$HOSTN")
+		`$cria_filho`
+		echo 4
 	done
 done
- 
-	
+}
+
+help() {
+	echo "****************************************************"
+	echo ""
+	echo " all_groups(get_hostgroups) - pega todos os grupos de hosts"
+	echo " all_host_per_groups(get_hosts_hostgroups) - pega todos os hosts de um grupo - \$2 (nome do grupo)"
+	echo " all_items_per_host(get_items_hosts) - pega todos os items com trigger de um host - \$2(nome do grupo) \$3(nome do host)"
+	echo " all_triggers_per_host(get_triggers_hosts) - pega todas as triggers de um host - \$2(nome do grupo) \$3(nome do host)"
+	echo ""
+	echo " cria_pai(mk_father_itservices) - criar pai \$2(nome do grupo)"
+	echo " cria_filho(mk_child_itservices) - criar filho \$2(nome do grupo) \$3(nome do filho)"
+	echo " cria_item(mk_child_its_trigger) - criar item com trigger - \$2(nome do grupo) \$3(nome do filho) \$4(nome do item)"
+	echo ""
+	echo " populate(mk_populate) - criar ITservices de forma automatica"
+	echo " limpa(limpa_tudo) - limpa todo o itservice"
+	echo ""
+	echo "TODO:"
+	echo " ** Colocar mais de uma trigger por item"
+	echo " ** Cacular sla baseado no cliente"
+	echo ""
+	echo "***************************************************"
+}
+
+limpa_tudo() {
+
+serviceid=$(get_itservices | awk -v RS='{"' -F: '/^serviceid/ {print $2}' | awk -F\" '{print $2}')
+for del in `echo $serviceid`
+do
+
+	wget -O- -o /dev/null $API --header 'Content-Type: application/json-rpc' --post-data "{
+	\"jsonrpc\": \"2.0\",
+	\"method\": \"service.delete\",
+	\"params\": [ $del ],
+	\"auth\": \"$AUTH_TOKEN\",
+	\"id\": 1 }"
+done
+
+}
+
+case $1 in
+	all_groups) get_hostgroups;
+	;;
+	all_host_per_groups) get_hosts_hostgroups;
+	;;
+	all_items_per_host) get_items_hosts;
+	;;
+	all_triggers_per_host) get_triggers_hosts;
+	;;
+	cria_pai) mk_father_itservices;
+	;;
+	cria_filho) mk_child_itservices;
+	;;
+	cria_item) mk_child_its_trigger;
+	;;
+	gs) get_itservices;
+	;;
+	populate) mk_populate;
+	;;
+	limpa) limpa_tudo;
+	limpa_tudo;
+	;;
+	*) help;
+	;;
+esac
